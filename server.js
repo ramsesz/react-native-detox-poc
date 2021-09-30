93a972088e976df2d3db141ab31a6f2dce420f63
Ramses Zeulevoet <ramses@ramsesz.be>
2021-09-30 16:52:54 +0200

    refactor: cleanup server file

--- server.js
+++ server.js
@@ -2,31 +2,21 @@ const {ApolloServer, gql} = require('apollo-server');
 const {MOCK_BOXES} = require('./mocks/boxes');
 
 // A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Box {
     id: String
     title: String
   }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     boxes: [Box]
   }
 `;
 
-const boxes = MOCK_BOXES;
-
 // Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
+// schema. This resolver retrieves boxes from the MOCK_BOXES response.
 const resolvers = {
   Query: {
+    boxes: () => MOCK_BOXES,
     boxes: () => boxes,
   },
 };
@@ -35,20 +25,11 @@ const createServerWithMockedSchema = (customMocks = null) => {
   const opts = {typeDefs, resolvers};
 
   if (customMocks) {
-    //@ts-ignore
     opts.mocks = customMocks;
   }
-  const server = new ApolloServer(opts);
 
-  return server;
+  return new ApolloServer(opts);
 };
-
-// const server = createServerWithMockedSchema();
-
-// // The `listen` method launches a web server.
-// server.listen().then(({url}) => {
-//   console.log(`🚀  Server ready at ${url}`);
-// });
 
 module.exports = {
   createServerWithMockedSchema,
